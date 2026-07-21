10a79a2ac95f7fe76d19737e7a05d261e72a35a7
mabdullah356 <abdullahworld111@gmail.com>
2026-07-21 21:47:41 +0500

    fix: add res.headersSent guard to error handler middleware

--- server/src/Middlewares/errorHandler.js
+++ server/src/Middlewares/errorHandler.js
@@ -1,5 +1,8 @@
 const errorHandler = (error,req,res,next)=>{
-    console.log(error);
+    console.error(error);
+    if (res.headersSent) {
+        return next(error);
+    }
     return res.status(500).json({message:"Internal server error"})
 }
 
